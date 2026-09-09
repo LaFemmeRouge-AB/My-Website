08e83f56b13f76652ac40a987d681c92e5acbc5c
LaFemmeRouge-AB <Adelaide.bornn@gmail.com>
2026-09-08 21:02:03 -0700

    Did next vinyl!

--- aboutme.js
+++ aboutme.js
@@ -22,3 +22,28 @@ target.addEventListener('click', () => {
         target.classList.remove('Playing');
     }
 });
+
+const prescription = document.getElementById('prescription-vinyl');
+const prescriptionAudio = document.getElementById('prescription-audio');
+
+prescription.addEventListener('click', () => {
+    if (prescription.classList.contains("is-spinning")) {
+        const computedStyle = window.getComputedStyle(prescription);
+        const currentTransform = computedStyle.transform;
+        prescription.classList.remove("is-spinning");
+        prescription.style.transform = currentTransform;
+    } else {
+        prescription.style.transform = '';
+        prescription.classList.add("is-spinning");
+    }
+});
+
+prescription.addEventListener('click', () => {
+    if (prescriptionAudio.paused) {
+        prescriptionAudio.play();
+        prescription.classList.add('Playing');
+    } else {
+        prescriptionAudio.pause();
+        prescription.classList.remove('Playing');
+    }
+});
